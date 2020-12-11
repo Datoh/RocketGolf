fb87ff70da093bdaf83b84d9041124ec0012be5f
Datoh <datoh44@gmail.com>
2020-12-11 16:49:35 +0100

    [bug] Fix unused signal teleport

--- scripts/Teleport.gd
+++ scripts/Teleport.gd
@@ -8,4 +8,3 @@ func _on_Area_body_entered(body: Node) -> void:
   player.global_transform.origin = destination.global_transform.origin
   player.global_transform.origin.y += player_transform_y
   player.look_at(player.global_transform.origin + destination.direction(), Vector3.UP)
-  emit_signal("teleport", body, destination)
