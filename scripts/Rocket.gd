9d87d5ef919d2dec3a9d7d2943c636adb4ba607e
Datoh <datoh44@gmail.com>
2020-12-13 12:30:04 +0100

    [physic] Fix rocket collision

--- scripts/Rocket.gd
+++ scripts/Rocket.gd
@@ -1,11 +1,15 @@
-extends Area
+extends KinematicBody
 
 export(float) var speed := 25.0
 export(float) var blow_radius := 3.0
-export(float) var blow_force := 20.0
+export(float) var blow_force_direct := 20.0
+export(float) var blow_force_indirect := 20.0
+export(float) var factor_add_velocity_hit_directly := 0.5
+export(float) var factor_add_up_hit_indirectly := 1.0
 
 var velocity := Vector3.ZERO
 var colliding_body: Spatial = null
+var colliding_position := Vector3.ZERO
 
 signal rocket_blow(rocket)
 
@@ -15,42 +19,39 @@ func launch(origin: Transform, direction: Transform) -> void:
   transform = direction
   velocity = -transform.basis.z * speed
 
+
 func hit(body: Spatial, position: Vector3) -> bool:
-  var hit := colliding_body == body
+  var hit_directly := colliding_body == body
+  var hit := hit_directly
+
   var distance := 0.0
-  var collider_point := global_transform.origin
-  
-  # first check if body is overlapping rocket
-  if !hit:
-    for overlapping_body in get_overlapping_bodies():
-      hit = hit || overlapping_body == body
   
   # else check if near 
   if !hit:
-    var raycast = $RayCast
-    raycast.cast_to = Vector3.ZERO
-    raycast.cast_to.z = blow_radius
-    raycast.force_raycast_update()
-    if colliding_body == raycast.get_collider():
-      raycast.global_transform.origin = raycast.get_collision_point()
-      raycast.transform.origin -= velocity.normalized() * 0.1
-     
+    var raycast := $RayCast
     raycast.look_at(position, Vector3.UP)
-    raycast.cast_to.z = -blow_radius
+    raycast.cast_to = Vector3(0.0, 0.0, -blow_radius)
     raycast.force_raycast_update()
-    hit = body == raycast.get_collider()
+    var colliding_ball = raycast.get_collider()
+    hit = colliding_ball != null && body == colliding_ball.get_parent()
     if hit:
-      collider_point = raycast.get_collision_point()
-      distance = collider_point.distance_to(global_transform.origin)
+      colliding_position = raycast.get_collision_point()
+      distance = colliding_position.distance_to(global_transform.origin)
       hit = distance < blow_radius
 
     raycast.global_transform.origin = global_transform.origin  
 
   if hit:
-    var impulse_direction = global_transform.origin.direction_to(position)
-    var impulse_force = (blow_radius - distance) / blow_radius
-    # far is the explosion, lower is the impulse
+    var impulse_direction := global_transform.origin.direction_to(position).normalized()
+    if !hit_directly:
+      impulse_direction += Vector3.UP * factor_add_up_hit_indirectly
+    else:
+      impulse_direction += velocity.normalized() * factor_add_velocity_hit_directly
+
+    var impulse_force := (blow_radius - distance) / blow_radius
+    # farest is the explosion, lower is the impulse
     impulse_force = ease(impulse_force, 2.6) # see https://godotengine.org/qa/59172/how-do-i-properly-use-the-ease-function
+    var blow_force := blow_force_direct if hit_directly else blow_force_indirect
     body.apply_central_impulse(impulse_direction.normalized() * impulse_force * blow_force)
     
   return hit
@@ -61,15 +62,14 @@ func _on_Timer_timeout() -> void:
 
 
 func _physics_process(delta: float) -> void:
-  transform.origin += velocity * delta
-
-
-func _on_Rocket_body_entered(body: Node) -> void:
-  if colliding_body != null: # already collide
-    return
-
-  if body.is_in_group("jumper_trigger"):
-    body.do_jump()
-  colliding_body = body
-  emit_signal("rocket_blow", self)
-  queue_free()
+  var colliding_info = move_and_collide(velocity * delta)
+  if colliding_info != null:
+    colliding_body = colliding_info.collider
+    print(colliding_body.name)
+    if colliding_body.is_in_group("ball"):
+      colliding_body = colliding_body.get_parent()
+    colliding_position = colliding_info.position
+    if colliding_body.is_in_group("jumper_trigger"):
+      colliding_body.do_jump()
+    emit_signal("rocket_blow", self)
+    queue_free()
