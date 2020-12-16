be3f1bd5f5a1ddecf2b35259c17d373d94e8d886
Datoh <datoh44@gmail.com>
2020-12-16 23:14:52 +0100

    [display] Fix score display

--- scripts/Hud.gd
+++ scripts/Hud.gd
@@ -18,7 +18,7 @@ func set_help() -> void:
 func draw_victory() -> void:
   $Timer.stop()
   var seconds = Global.level_time % 60
-  var minutes = Global.level_time - seconds
+  var minutes = (Global.level_time - seconds) / 60
   find_node("VictoryTimeLabel").text = "Time: " + str(minutes) + ":" + str(seconds).pad_zeros(2)
   find_node("VictoryRocketLabel").text = "Rockets: " + str(Global.level_rockets_count)
   find_node("VictoryScoreLabel").text = "Score: " + str(Global.level_score) + "%"
@@ -29,7 +29,7 @@ func draw_victory() -> void:
 func _on_Timer_timeout() -> void:
   Global.level_time += 1
   var seconds = Global.level_time % 60
-  var minutes = Global.level_time - seconds
+  var minutes = (Global.level_time - seconds) / 60
   hud_time.text = str(minutes) + ":" + str(seconds).pad_zeros(2)
 
 
