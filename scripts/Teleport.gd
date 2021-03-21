aaf2a33020060c09d81fdff09abf595816432fbf
Datoh <datoh44@gmail.com>
2021-03-21 21:50:20 +0100

    [teleport] Rename teleport signal method

--- scripts/Teleport.gd
+++ scripts/Teleport.gd
@@ -1,6 +1,6 @@
 extends CSGBox
 
-func _on_Area_body_entered(body: Node) -> void:
+func _on_TeleportArea_body_entered(body: Node) -> void:
   var destination = get_tree().root.find_node(name + "Destination", true, false)
   var player := body as KinematicBody
   var player_transform_y = player.global_transform.origin.y - global_transform.origin.y
